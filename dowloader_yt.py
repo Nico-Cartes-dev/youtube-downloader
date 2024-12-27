9090985b8c8a19a8b6f8aa968c4855e5fe776c81
Nicolas cartes <78509370+Nico-Cartes-dev@users.noreply.github.com>
2024-12-27 12:51:14 -0300

    Update dowloader_yt.py

--- dowloader_yt.py
+++ dowloader_yt.py
@@ -3,8 +3,14 @@ import os
 
 video_url = input("Enter the video URL: ")
 option = input("Choose video (1) or audio (2): ").strip()
-# download_path = r"choose your route"  
+download_path = r"L:\video"  
 os.makedirs(download_path, exist_ok=True)
+
+def get_name(url):
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        return info_dict.get('title', 'Unknown Title')
 
 def audio_quality(audio_format, ydl_opts):
     if audio_format == '1':
@@ -48,6 +54,8 @@ def audio_quality(audio_format, ydl_opts):
 
 
 try:
+    video_title = get_name(video_url)
+    print(f'Video title: {video_title}')
     ydl_opts = {
         'ffmpeg_location': r'C:\ffmpeg\bin',
         'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),  
@@ -55,7 +63,17 @@ try:
 
     if option == '1':
         print('You have chosen video.')
-        ydl_opts['format'] = 'best'
+        video_format = input("Choose video format (1: best, 2: mp4): ").strip()
+        if video_format == '1':
+            print('Chosen format: Best available')
+            ydl_opts['format'] = 'bestvideo+bestaudio/best'
+        elif video_format == '2':
+            print('Chosen format: MP4')
+            ydl_opts['format'] = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]'
+            ydl_opts['merge_output_format'] = 'mp4'
+        else:
+            print('Invalid option, using best available format.')
+            ydl_opts['format'] = 'bestvideo+bestaudio/best'
     elif option == '2':
         print('You have chosen audio.')
         audio_format = input("Choose audio format (1: mp3, 2: wav, 3: flac): ").strip()
